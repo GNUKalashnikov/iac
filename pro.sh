1dc5d75730bc174dd04ab713cc2d8936260d50bc
Ivan Kalashnikov <ivan@kalashnikov.uk>
2022-01-04 16:47:33 +0000

    added provisioning files

---
+++ pro.sh
@@ -0,0 +1,8 @@
+# Update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install
+sudo apt-get install software-properties-common
+sudo add-repository ppa:ansible/ansible -y
+sudo apt-get install ansible -y
